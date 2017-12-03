ccc8fd88eb93ff96050852da8ac5fe0a842e4eea
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2017-12-03 16:53:56 +0900

    [Change] - get the selection text with HTML tag

--- src/js/lib/HTMLMetaDataScraper.js
+++ src/js/lib/HTMLMetaDataScraper.js
@@ -53,6 +53,23 @@ export default class HTMLMetaDataScraper {
     if (type === 'image') {
       return null;
     } else if (type === 'text') {
+      const selectedElement = window.getSelection().getRangeAt(0);
+      const startParentElement = selectedElement.startContainer.parentElement;
+      const endParentElement = selectedElement.endContainer.parentElement;
+      const $start = $(startParentElement);
+      const $end = $(endParentElement);
+
+      if (startParentElement === endParentElement) {
+        return this.removeScriptTag($start.html());
+      } else {
+        // jquery - get a common parent for two DOM elements - Stack Overflow
+        // https://stackoverflow.com/questions/7647864/get-a-common-parent-for-two-dom-elements
+        const $commonParent = $start
+          .parents()
+          .has(endParentElement)
+          .first();
+        return this.removeScriptTag($commonParent.html());
+      }
       return this.data.selectionText;
     } else {
       let content = null;
