c7c467f380eba9c6ef97b3689b9255e4b416e7d7
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2018-04-06 01:01:01 +0900

    fixed #14

--- src/js/lib/Insertion/supportService/GelbooruKawpaaLinkInsertion.js
+++ src/js/lib/Insertion/supportService/GelbooruKawpaaLinkInsertion.js
@@ -17,6 +17,6 @@ export default class GelbooruKawpaaLinkInsertion extends KawpaaLinkInsertion {
   }
 
   normalize(src) {
-    return `https:${src}`;
+    return src.includes('https:') ? src : `https:${src}`;
   }
 }
