8a2a58f4e311dfb7af6f82c5f1bb83af492c9563
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2018-05-19 21:24:20 +0900

    Fix a bug that kawpaa button is not shown in DeviantArt

--- src/js/lib/Insertion/supportService/DeviantArtKawpaaLinkInsertion.js
+++ src/js/lib/Insertion/supportService/DeviantArtKawpaaLinkInsertion.js
@@ -1,33 +1,77 @@
 import $ from 'jquery';
-import { SUPPORT_SERVICE } from '../../../config/config';
+import { SUPPORT_SERVICE, ICONS } from '../../../config/config';
 import KawpaaLinkInsertion from '../KawpaaLinkInsertion';
 
 export default class DeviantArtKawpaaLinkInsertion extends KawpaaLinkInsertion {
   constructor() {
     super(SUPPORT_SERVICE.DEVIANTART_HOSTNAME);
     this.selector = '.dev-meta-actions';
-    this.html = `\
-      <a class="dev-page-button dev-page-button-with-text dev-page-download kawpaa-save-link" href="#" data-download_url="http://www.deviantart.com/download/460347620/gochiusa_by_azizkeybackspace-d7m2uhw.png?token=a6e80ce8b02c8c1dc7762417c29bf3d3b57bd13d&amp;ts=1458132778">
-      <i style="background: url(${
-        ICONS.BLUE_16
-      }); background-position: none; background-repeat: no-repeat;"></i>
-      <span class="label">Save to Kawpaa</span>
-      </a>\
-    `;
 
-    this.onChangeURL();
+    // this.onChangeURL();
+  }
+
+  buildButton({ ext, width, height, imageUrl }) {
+    this.html = `
+    <a class='dev-page-button dev-page-button-with-text dev-page-download kawpaa-save-link' href='#' data-download_url='${imageUrl}'>
+    <i style='background: url(${
+      ICONS.BLUE_16
+    }); background-position: none; background-repeat: no-repeat;'></i>
+    <span class='label'>Save to Kawpaa</span>
+    <span class='text'>${ext} ${width} x ${height}</span>
+    </a>
+    `;
+  }
+
+  extractImageInfo() {
+    const img = $('.dev-view-deviation  .dev-content-full');
+    const width = img.attr('width');
+    const height = img.attr('height');
+    const imageUrl = img.attr('src');
+
+    const filename_ext = /.+\/(.+?)([\?#;].*)?$/;
+    const filename = imageUrl.match(filename_ext)[1];
+    const ext_reg = /(.*)(?:\.([^.]+$))/;
+    const ext = filename.match(ext_reg)[2];
+
+    return {
+      ext: ext.toUpperCase(),
+      width,
+      height,
+      imageUrl,
+    };
+  }
+
+  insert() {
+    const imageInfo = this.extractImageInfo();
+    console.log(imageInfo);
+    this.buildButton(imageInfo);
+    super.insert();
+    return this;
+  }
+
+  replace() {
+    const hasKawpaaSaveLink = $('.kawpaa-save-link').length > 0;
+    console.log($('.dev-page-download'));
+    if (hasKawpaaSaveLink) {
+      $('.kawpaa-save-link').remove();
+    }
+    const _this = this;
+    setTimeout(() => {
+      _this.insert();
+    }, 1000);
   }
 
   onChangeURL() {
-    $(document).on('change', 'data-deviationid', () => {
-      const sampleImgUrl = $(this).attr('src');
-      return console.log('sampleImgUrl = ', sampleImgUrl);
-    });
+    const _this = this;
+    setInterval(() => {
+      console.log('Replace');
+      _this.replace();
+    }, 1000);
   }
 
   getUrl() {
     return new Promise(resolve => {
-      const srcUrl = $('.dev-content-full').attr('src');
+      const srcUrl = $('.dev-view-deviation  .dev-content-full').attr('src');
       return resolve(srcUrl);
     });
   }
