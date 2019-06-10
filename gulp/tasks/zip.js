dc7b6bc48e585c00f56e75348cbe47bdda189e93
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2019-06-11 00:46:24 +0900

    Fix a bug that zip is not created

--- gulp/tasks/zip.js
+++ gulp/tasks/zip.js
@@ -6,15 +6,18 @@ const watch = require('gulp-watch');
 
 const ManifestVersionManager = require('../lib/ManifestVersionManager');
 
-gulp.task('zip', gulp.series('update_manifest_version'), function() {
-  let manifest = require('../../manifest.json');
-  let newVersion = new ManifestVersionManager(argv.versions, manifest.version)
-    .update()
-    .getVersion();
-  let distFileName = `${manifest.name} v${newVersion}.zip`;
-  console.log(distFileName);
-  return gulp
-    .src(['build/**/*', 'manifest.json'], { base: '.' })
-    .pipe($.zip(distFileName))
-    .pipe(gulp.dest('dist'));
-});
+gulp.task(
+  'zip',
+  gulp.series('update_manifest_version', function() {
+    let manifest = require('../../manifest.json');
+    let newVersion = new ManifestVersionManager(argv.versions, manifest.version)
+      .update()
+      .getVersion();
+    let distFileName = `${manifest.name} v${newVersion}.zip`;
+    console.log(distFileName);
+    return gulp
+      .src(['build/**/*', 'manifest.json'], { base: '.' })
+      .pipe($.zip(distFileName))
+      .pipe(gulp.dest('dist'));
+  }),
+);
