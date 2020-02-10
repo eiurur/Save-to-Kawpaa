850365f13f950848f33ea638109e79207f2ec0da
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2020-02-10 16:24:59 +0900

    Add naming variable

--- src/js/lib/metaData/LinkScraper.js
+++ src/js/lib/metaData/LinkScraper.js
@@ -18,10 +18,16 @@ export default class LinkScraper extends HTMLMetaDataScraper {
    * imageならnull
    */
   getContent() {
-    let content = null;
+    const isYoutube = location.href.indexOf('www.youtube.com/watch?v=') > -1;
+    const isNicoNico = location.href.indexOf('www.nicovideo.jp/watch/sm') > -1;
+    const is2ch =
+      location.href.indexOf('bbspink.com') > -1 ||
+      location.href.indexOf('2ch.net') > -1;
+    const is2chan = location.href.indexOf('2chan.net') > -1;
+    const isPornhub = location.href.indexOf('pornhub.com') > -1;
 
-    // youtube
-    if (location.href.indexOf('www.youtube.com/watch?v=') > -1) {
+    let content = null;
+    if (isYoutube) {
       let vNumber = location.search
         .split('&')
         .shift()
@@ -30,84 +36,54 @@ export default class LinkScraper extends HTMLMetaDataScraper {
       content = `\
           <iframe width="560" height="315" src="https://www.youtube.com/embed/${vNumber}" frameborder="0" allowfullscreen></iframe>\
         `;
-
+      return content;
+    }
+    if (isNicoNico) {
       // ニコニコなら動画のサムネを指定
       // # ニコニコはhttpsに対応していないのでiframeがブロックされます。どうしようもないので待ちましょう。
-    } else if (location.href.indexOf('www.nicovideo.jp/watch/sm') > -1) {
-      let { title } = document;
-      let smNumber = location.href.split('/').pop();
+      const { title } = document;
+      const smNumber = location.href.split('/').pop();
       content = `\
           <iframe width="312" height="176" src="//ext.nicovideo.jp/thumb/${smNumber}" scrolling="no" style="border:solid 1px #CCC;" frameborder="0"><a href="//www.nicovideo.jp/watch/${smNumber}">${title}</a></iframe>\
         `;
-
-      // 2ch
-    } else if (
-      location.href.indexOf('bbspink.com') > -1 ||
-      location.href.indexOf('2ch.net') > -1
-    ) {
+      return content;
+    }
+    if (is2ch) {
       content = this.removeScriptTag($('.thread').html());
-
-      // ふたば
-    } else if (location.href.indexOf('2chan.net') > -1) {
+      return content;
+    }
+    if (is2chan) {
       content = this.removeScriptTag($('.thre').html());
-
-      // Just for me
-    } else if (location.href.indexOf('mannanoeroetaiken.blog.fc2.com') > -1) {
-      content = this.removeTag(
-        'img',
-        this.removeTag(
-          'iframe',
-          this.removeHrefInATag(this.removeScriptTag($('.content').html())),
-        ),
-      );
-
-      // Pornhub
-    } else if (location.href.indexOf('pornhub.com') > -1) {
-      var params = QueryStringParser.parse();
-      content = `<iframe src="https://jp.pornhub.com/embed/${params.viewkey}" frameborder="0" width="560" height="340" scrolling="no" allowfullscreen></iframe>`;
-    } else {
-      content = this.removeScriptTag($('main').html());
-      console.log(content);
+      return content;
     }
 
+    if (isPornhub) {
+      const params = QueryStringParser.parse();
+      content = `<iframe src="https://jp.pornhub.com/embed/${params.viewkey}" frameborder="0" width="560" height="340" scrolling="no" allowfullscreen></iframe>`;
+      return content;
+    }
+
+    content = this.removeScriptTag($('main').html());
     return content;
   }
 
   getURL() {
-    let u;
+    const ogImage = this.getSiteImage();
+    if (ogImage) return ogImage;
 
-    let siteURL = this.getSiteURL();
-
-    // デフォ値の設定
-    let $img = $('img');
-    let ogImage = this.getSiteImage();
-
-    if (ogImage) {
-      return ogImage;
-    } else if ($img != null && $img.length > 0) {
-      // bodyタグ内で一番最初の画像を引っ張ってくる
-      console.log('画像ファイル発見', $img);
-      let firstImgUrlInBody = $img.get(0).src;
-      // 存在しないなら初期値を設定する
-      // (ex) Twitterだと、"$img.get(0) = <img class=​"avatar size32" alt>​"とかになる。srcがねえ。
-      u = firstImgUrlInBody || DEFAULT_IMAGE_URL;
-    } else {
-      // ページに画像が存在しない場合は灰色の画像を代わりに使用
-      console.log('画像ファイルが見つからない。');
-      u = DEFUALT_URL;
-    }
+    let u = DEFAULT_IMAGE_URL;
+    const $img = $('img');
 
     /*
     ここから例外処理(特別処理？)
     */
     // 例外中の例外。もし、他のChromeExtensionがimgを挿入していた場合、urlにchrome-extension://から始まる画像ファイルが代入され、保存に失敗してしまう。
-    if (
+    const isAtChromeExtensionPage =
       this.__guard__(
         this.__guard__(this.data, x1 => x1.url),
         x => x.indexOf('chrome-extension://'),
-      ) > -1
-    ) {
-      console.log('ChromeExnteionsファイルを画像に設定されてしまった。');
+      ) > -1;
+    if (isAtChromeExtensionPage) {
       u = $img.get(1).src;
     }
 
@@ -117,6 +93,8 @@ export default class LinkScraper extends HTMLMetaDataScraper {
     //     $('.videoThumbnailImage').attr('src') || // Flash版プレイヤー
     //     $('.is-playing .Image').attr('src'); // HTML5版プレイヤー
     // }
+
+    let siteURL = this.getSiteURL();
 
     // youtube
     const youtubeMatch = /https:\/\/www.youtube.com\/watch\?v=([a-zA-Z0-9\-\_]{11})/.exec(
